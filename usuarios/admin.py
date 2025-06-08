1b41c449d48250ae5bcb06f500ed978b267c6042
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-06-08 18:42:35 -0300

    feat: criando o usuario base de autenticação, sobrescrevendo o forms e adicionando ao admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Users
+from django.contrib.auth import admin as adm_auth
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+@admin.register(Users)
+class UsersAdmin(adm_auth.UserAdmin):
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = Users
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Users(AbstractUser):
+
+    CARGO_GERENTE = 'G'
+    CARGO_VENDEDOR = 'V'
+
+    CHOICES_CARGO = (
+        (CARGO_VENDEDOR, 'Vendedor'),
+        (CARGO_GERENTE, 'Gerente'),
+    )
+
+    cargo = models.CharField(
+        max_length=1,
+        choices=CHOICES_CARGO
+    )
+
+    
