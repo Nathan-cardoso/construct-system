e52d71adf5bfac9977baf1c006a0ef946502573a
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-06-08 19:00:10 -0300

    feat: adicionando o campo cargo ao painel de admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -3,10 +3,13 @@ from .models import Users
 from django.contrib.auth import admin as adm_auth
 from .forms import UserChangeForm, UserCreationForm
 
-
 @admin.register(Users)
+
 class UsersAdmin(adm_auth.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
     model = Users
+    fieldsets = adm_auth.UserAdmin.fieldsets + (
+        ('Cargo', {'fields': ('cargo',)}),
+    )
