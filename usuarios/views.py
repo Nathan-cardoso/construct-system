03abce62edcc111d1b98dba183db0ba1fab832ff
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-06-08 21:20:12 -0300

    feat: criando a pagina de cadastro de vendedor, atribuindo o cargo e permissões pelo signal e adicionando o signals ao app

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rolepermissions.decorators import has_permission_decorator
+from .models import Users
 
 @has_permission_decorator('cadastrar_vendedores')
 def cadastrar_vendedor(request):
-    return HttpResponse('teste')
+    if request.method == 'GET':
+        return render(request, 'cadastrar_vendedor.html')
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = Users.objects.filter(email=email)
+
+        if user.exists():
+            return HttpResponse('Usuário já cadastrado')
+        
+        user = Users.objects.create_user(
+            username=email,
+            email=email,
+            password=senha,
+            cargo="V"
+        )
+
+        return HttpResponse('Usuário cadastrado com sucesso')
